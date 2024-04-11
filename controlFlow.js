f5b497c7f7c63b569dbd1560b038d265ba42e797
Larysa Rud <larpavrud@gmail.com>
2024-04-10 22:02:45 -0400

    set up

---
+++ controlFlow.js
@@ -0,0 +1,15 @@
+//Part 1: Growing Pains
+
+//The area in which the plants are contained is circular, 
+//with a radius of 5 meters
+let radius = 5;
+const pi = 3.1415;
+//calculating the area of a circle, square meters
+const area = pi*radius*radius;
+console.log(area);
+
+//Each plant requires a minimum space of 0.8 square meters
+plantMinspace = 0.8;
+
+//The area is starting with 20 plants
+startPlants = 20;
