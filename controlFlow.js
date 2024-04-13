2b238b29fda974d5aeceeff28f9a1b0385ee1b64
Larysa Rud <larpavrud@gmail.com>
2024-04-12 20:26:34 -0400

    Part 3: Errors in Judgement

--- controlFlow.js
+++ controlFlow.js
@@ -5,8 +5,8 @@
 let radius = 5;
 const pi = 3.1415;
 //calculating the area of a circle, square meters
-const area = Math.round(pi*radius*radius);
-console.log("Area of the whole feeld is " + area);
+const area = pi*radius*radius;
+console.log("Area of the whole field is " + area);
 
 //Each plant requires a minimum space of 0.8 square meters
 onePlantMinspace = 0.8;
@@ -50,11 +50,9 @@ console.log("50% of the maximum capacity of the garden " + maxCapacity50);
 
 if (allPlantsArea > maxCapacity80)
 {
- console.log("Pruned them");   
+ console.log("Prune them");   
 } else if (allPlantsArea >= maxCapacity50){
 console.log("Monitor them");   
-} else if (allPlantsArea == maxCapacity80){
-    console.log("Monitor them")  
 } else {
 console.log("Plant them")
 }
@@ -64,11 +62,9 @@ console.log("Plant them")
 
 if (allPlantsAreaWeek2 > maxCapacity80)
 {
- console.log("Pruned them");   
+ console.log("Prune them");   
 } else if (allPlantsAreaWeek2 >= maxCapacity50){
 console.log("Monitor them");   
-} else if (allPlantsAreaWeek2 == maxCapacity80){
-    console.log("Monitor them")  
 } else {
 console.log("Plant them")
 }
@@ -78,16 +74,50 @@ console.log("Plant them")
 
 if (allPlantsAreaWeek3 > maxCapacity80)
 {
- console.log("Pruned them");   
+ console.log("Prune them");   
 } else if (allPlantsAreaWeek3 >= maxCapacity50){
 console.log("Monitor them");   
-} else if (allPlantsAreaWeek3 == maxCapacity80){
-    console.log("Monitor them")  
 } else {
 console.log("Plant them")
 }
 
+//Part 2: Thinking Bigger
 
+/*The conservation area in which the garden is located 
+has multiple other gardens. 
+Using the logic you have already created, determine:
+The amount of additional space that would be required 
+if the scientists were to start with 100 plants, 
+and did not prune them for 10 weeks.
+If the space remained circular, 
+what would be the radius of this expanded garden?*/
+
+
+/*The amount of additional space that would be required 
+if the scientists were to start with 100 plants, 
+and did not prune them for 10 weeks.*/
+
+/*The number of plants at the end of 10 weeks
+if the scientists were to start with 100 plants, 
+and did not prune them for 10 weeks */
+
+let plantNotPrunedAfter10Weeks  = 100 * (Math.pow(2, 10));
+console.log(`Number of plants if will not pruned garden after 10 Weeks is ${plantNotPrunedAfter10Weeks}`);
+
+//Area of this expanded garden is
+areaExpandGarden = plantNotPrunedAfter10Weeks * onePlantMinspace;
+console.log('The area of this expanded garden is ' + areaExpandGarden);
+
+additionalSpace = areaExpandGarden - area;
+console.log('The additional space is ' + additionalSpace);
+
+/*If the space remained circular, 
+what would be the radius of this expanded garden?*/
+radiusSquaredForExpandGarden = areaExpandGarden / pi;
+console.log('Radius squared for expanded garden is ' + radiusSquaredForExpandGarden);
+
+radiusForExpandGarden = Math.sqrt(radiusSquaredForExpandGarden);
+console.log('Radius of this expanded garden is ' + radiusForExpandGarden);
 
 
 
@@ -97,14 +127,17 @@ console.log("Plant them")
 and have instead started 
 with 100 plants in the original 5-meter-radius garden.*/
 
-let x = 100 * onePlantMinspace;
+let plants = 100;
 
 try {
-	if (x > area) {
-		console.log("Not enough space!");
-	} else {
-		throw "Enough space!";
-	}
+		x = plants * onePlantMinspace;
+		if (x <= area) {
+			console.log(`Enough space!`);
+		} else {
+			let message = `Not enough space!`;
+			throw message;
+		}
+	
 } catch (error) {
 	console.log(error);
 }
